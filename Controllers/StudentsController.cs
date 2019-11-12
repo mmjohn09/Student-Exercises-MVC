6cb3ceaa8a16bb269b44f8da3fe9dc496e272789
Maggie Johnson <mmjohn09@icloud.com>
2019-11-12 15:30:13 -0600

    added student details view

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -68,7 +68,40 @@ namespace StudentExercisesMVC.Controllers
         // GET: Students/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT s.Id,
+                                        s.FirstName,
+                                        s.LastName,
+                                        s.SlackHandle,
+                                        s.CohortId
+                                        FROM Students s
+                                        WHERE Id = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Student student = null;
+                    if (reader.Read())
+                    {
+                        student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+
+                        };
+                    }
+                    reader.Close();
+
+                    return View(student);
+                }
+            }
         }
 
         // GET: Students/Create
